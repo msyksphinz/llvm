2f80567639f65abd0c86d52acd96f93140cd0569
msyksphinz <msyksphinz_dev@gmail.com>
2019-09-05 22:36:04 +0800

    Implement MYRISCVX/MYRISCVXSubtarget.* and update MYRISCVX/MYRISCVX.td

---
+++ lib/Target/MYRISCVX/MYRISCVXSubtarget.cpp
@@ -0,0 +1,93 @@
+//===-- MYRISCVXSubtarget.cpp - MYRISCVX Subtarget Information ------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements the MYRISCVX specific subclass of TargetSubtargetInfo.
+//
+//===----------------------------------------------------------------------===//
+
+#include "MYRISCVXSubtarget.h"
+
+#include "MYRISCVXMachineFunction.h"
+#include "MYRISCVX.h"
+#include "MYRISCVXRegisterInfo.h"
+
+#include "MYRISCVXTargetMachine.h"
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/TargetRegistry.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "MYRISCVX-subtarget"
+
+#define GET_SUBTARGETINFO_TARGET_DESC
+#define GET_SUBTARGETINFO_CTOR
+#include "MYRISCVXGenSubtargetInfo.inc"
+
+extern bool FixGlobalBaseReg;
+
+void MYRISCVXSubtarget::anchor() { }
+
+//@1 {
+MYRISCVXSubtarget::MYRISCVXSubtarget(const Triple &TT, const std::string &CPU,
+                                     const std::string &FS,
+                                     const MYRISCVXTargetMachine &_TM) :
+    //@1 }
+    // MYRISCVXGenSubtargetInfo will display features by llc -march=MYRISCVX -mcpu=help
+    MYRISCVXGenSubtargetInfo(TT, CPU, FS),
+    TM(_TM), TargetTriple(TT), TSInfo(),
+    InstrInfo(),
+    FrameLowering(initializeSubtargetDependencies(CPU, FS, TM)),
+    TLInfo(TM, *this), RegInfo(*this) {
+
+}
+
+
+bool MYRISCVXSubtarget::isPositionIndependent() const {
+  return TM.isPositionIndependent();
+}
+
+MYRISCVXSubtarget &
+MYRISCVXSubtarget::initializeSubtargetDependencies(StringRef CPU, StringRef FS,
+                                                   const TargetMachine &TM) {
+  if (TargetTriple.getArch() == Triple::myriscvx32) {
+    if (CPU.empty() || CPU == "generic") {
+      CPU = "cpu-rv32";
+    }
+  } else if (TargetTriple.getArch() == Triple::myriscvx64) {
+    if (CPU.empty() || CPU == "generic") {
+      CPU = "cpu-rv64";
+    }
+  } else {
+    errs() << "!!!Error, TargetTriple.getArch() = " << TargetTriple.getArch()
+           <<  "CPU = " << CPU << "\n";
+    exit(0);
+  }
+
+  // Parse features string.
+  ParseSubtargetFeatures(CPU, FS);
+  // Initialize scheduling itinerary for the specified CPU.
+  InstrItins = getInstrItineraryForCPU(CPU);
+
+  if (HasRV64 == true) {
+    XLenVT = MVT::i64;
+  } else {
+    XLenVT = MVT::i32;
+  }
+  return *this;
+}
+
+bool MYRISCVXSubtarget::abiUsesSoftFloat() const {
+  //  return TM->Options.UseSoftFloat;
+  return true;
+}
+
+const MYRISCVXABIInfo &MYRISCVXSubtarget::getABI() const { return TM.getABI(); }
